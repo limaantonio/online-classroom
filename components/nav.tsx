2d6fc3c5af7e4661216e46154ee2730714781bc5
limaantonio <limaantoniocarlos20@gmail.com>
2020-11-03 22:22:31 -0300

    first commit

---
+++ components/nav.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from 'next';
+import Link from 'next-link';
+
+const links = [
+  { href: 'https://github.com/vercel/next.js', label: 'Github' },
+  { href: 'https://nextjs.org/docs', label: 'Docs' },
+];
+
+const Nav(): NextPage = () => {
+  return (
+    <nav>
+      <ul className="flex justify-between items-center p-8">
+        <li>
+          <Link href="/">
+            <a href="" className="text-blue-500 no-underline">
+              Home
+            </a>
+          </Link>
+        </li>
+        <ul className="flex justify-between items-center space-x-4">
+          {links.map(({ href, label }) => (
+            <li key={`${href}${label}`}>
+              <a href="" className="btn-blue no-underline">
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
